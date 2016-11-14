16c6e2fc3cacf744b8fef076c023b73e14bf2f2b
Peter Bijker <peter.bijker@gmail.com>
2016-11-14 14:25:56 +0100

    First commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Samenweb</title>
+</head>
+<body>
+    <p>Hallo beste 2MD, we gaan samen een nieuw project via Git maken</p>
+    <p>Je maakt een nieuwe branch van dit project en doet een aanpassing</p>
+</body>
+</html>
